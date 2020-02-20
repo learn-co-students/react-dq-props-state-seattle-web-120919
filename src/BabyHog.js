ed63fa4831a29c0e2958807c00866e42778638a4
christinamcmahon <christina.mcmahon1@gmail.com>
2020-02-20 10:08:48 -0800

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
-import normalBaby from './assets/unadulterated-hoglette.png'
+// import normalBaby from './assets/unadulterated-hoglette.png'
 import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
@@ -8,10 +8,19 @@ import GlowingBaby from './assets/glowing-eyes.png'
 // is there a way we could associate eye color string values with images? 
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
+const eyeColorMapper = {
+  'sun': SunBaby,
+  'blue': BlueBaby,
+  'glowing': GlowingBaby
+}
+
 export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 10
+    }
   }
 
   changeWeight = (e) => {
@@ -25,22 +34,22 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>{this.props.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {this.props.hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
+
+        <Button name="+" onClick={this.changeWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={eyeColorMapper[this.props.eyeColor]} style={{ height: this.state.weight }} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
